c5fe419f921654c292d29222862f9b11bb158764
sung2708 <aki8082002@gmail.com>
2025-11-08 15:03:16 +0700

    fix(auth): fix error pass to short in model

--- internal/model/models.go
+++ internal/model/models.go
@@ -4,16 +4,16 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `gorm:"unique;not null"`
-	Email    string `gorm:"uniqueIndex;not null"`
-	Password string
+	Name     string `gorm:"unique;not null" json:"name"`
+	Email    string `gorm:"uniqueIndex;not null" json:"email"`
+	Password string `gorm:"not null" json:"password"`
 }
 
 type URL struct {
 	gorm.Model
-	LongURL    string `gorm:"not null"`
-	ShortCode  string `gorm:"uniqueIndex;not null"`
-	UserID     *uint
-	User       User `gorm:"foreignKey:UserID"`
-	ClickCount int  `gorm:"default:0"`
+	LongURL    string `gorm:"not null" json:"long_url"`
+	ShortCode  string `gorm:"uniqueIndex;not null" json:"short_code"`
+	UserID     *uint  `gorm:"not null" json:"user_id"`
+	User       User   `gorm:"foreignKey:UserID" json:"user"`
+	ClickCount int    `gorm:"default:0" json:"click_count"`
 }
